076429ef56f8f855bcd0dfcafc65a7ecac525bbd
Atharv Muthukumar <atharvmuthukumar@gmail.com>
2026-09-07 17:11:21 +0530

    sorting

--- task4.py
+++ task4.py
@@ -1,3 +1,15 @@
 str=input("Enter a string: ")
-sorted_str=sorted(str)
-print("Sorted string:", ''.join(sorted_str))
+q=list(str)
+l=len(str)
+ord=int(input("Enter 1 for ascending order sorting and 2 for descending order sorting: "))
+for i in range(0,l):
+  for j in range(0,l-1):
+    if(ord==1):
+      if (q[j]>q[j+1]):
+        q[j],q[j+1]=q[j+1],q[j]
+    elif(ord==2):
+      if (q[j]<q[j+1]):
+        q[j],q[j+1]=q[j+1],q[j]
+    else:
+      print("Please enter 1 or 2")
+print(''.join(q))
